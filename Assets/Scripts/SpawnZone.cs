ad78766e2bcf36ac76b904224987332126144b86
ObdulioBG-Rutgers <ob221@scarletmail.rutgers.edu>
2026-09-12 22:01:16 -0400

    PRE SCORE

--- Assets/Scripts/SpawnZone.cs
+++ Assets/Scripts/SpawnZone.cs
@@ -8,7 +8,8 @@ public class SpawnZone : MonoBehaviour
     public GameObject bomb;
     public GameObject golden_apple;
     private BoxCollider spawn_area;
-    private float apple_count = 0;
+    private float apple_to_bomb_count = 0;
+    private float apple_to_gold_count = 0;
 
     private float countdown = 30f;
     private float time = 0f;
@@ -37,15 +38,21 @@ public class SpawnZone : MonoBehaviour
         {
             float rand_chance = Random.Range(1, 10);
             GameObject random_apple;
-            
-            if (rand_chance == 1 || apple_count >= 10)
+
+            if (rand_chance == 1 || apple_to_bomb_count >= 10)
             {
                 random_apple = Instantiate<GameObject>(bomb);
-                apple_count = 0;
+                apple_to_bomb_count = 0;
+
+            } else if (apple_to_gold_count >= 15) {
+                random_apple = Instantiate<GameObject>(golden_apple);
+                apple_to_gold_count = 0;
+
             } else
             {
                 random_apple = Instantiate<GameObject>(apple);
-                apple_count += 1;
+                apple_to_bomb_count += 1;
+                apple_to_gold_count += 1;
             }
 
             random_apple.transform.position = new Vector3(Random.Range(spawn_area.bounds.min.x, spawn_area.bounds.max.x), transform.position.y, Random.Range(spawn_area.bounds.min.z, spawn_area.bounds.max.z));
